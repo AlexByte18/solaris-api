339300a3d1616ea9ca7d24e0f97fb6bc3d9c3efd
THAlexCortes <alex.g.cortes.g@gmail.com>
2026-01-23 22:39:40 -0600

    feat(users): add dtos and mapper class

---
+++ src/main/java/com/tipyme/solaris_api/users/domain/mapper/UserMapper.java
@@ -0,0 +1,28 @@
+package com.tipyme.solaris_api.users.domain.mapper;
+
+import java.util.List;
+
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import com.tipyme.solaris_api.users.User;
+import com.tipyme.solaris_api.users.domain.dto.UserRequestDto;
+import com.tipyme.solaris_api.users.domain.dto.UserResponseDto;
+import com.tipyme.solaris_api.users.domain.dto.UserUpdateDto;
+
+@Mapper(componentModel="spring")
+public interface UserMapper {
+    @Mapping(target="id", ignore=true)
+    User toEntity(UserRequestDto userRequestDto);
+    
+    UserResponseDto toResponseDto(User user);
+
+    List<UserResponseDto> toUserResponseDtoList(List<User> users);
+
+    @BeanMapping(nullValuePropertyMappingStrategy=NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target="id", ignore=true)
+    void updateUserFromDto(UserUpdateDto dto, @MappingTarget User user);
+}

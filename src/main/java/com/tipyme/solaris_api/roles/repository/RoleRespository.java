aa4fc6474c25b604dce20c540ceaa2f514779331
THAlexCortes <alex.g.cortes.g@gmail.com>
2026-01-28 10:39:32 -0600

    feat: add role repository

---
+++ src/main/java/com/tipyme/solaris_api/roles/repository/RoleRespository.java
@@ -0,0 +1,14 @@
+package com.tipyme.solaris_api.roles.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tipyme.solaris_api.roles.Role;
+
+@Repository
+public interface  RoleRespository extends JpaRepository<Role, Long> {
+     Optional<Role> getByName(String name);
+     Optional<Role> findByName(String name);
+}

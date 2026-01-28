c121bd3d83931c54713ab9bdf75c2dae76bfc6e0
THAlexCortes <alex.g.cortes.g@gmail.com>
2026-01-28 10:39:50 -0600

    feat(auth): update user repository

--- src/main/java/com/tipyme/solaris_api/users/repository/UserRepository.java
+++ src/main/java/com/tipyme/solaris_api/users/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.tipyme.solaris_api.users.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,11 @@ import com.tipyme.solaris_api.users.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+    Optional<User> getByUsername(String username);
+    Optional<User> findByUsername(String username);
+    Optional<User> getByEmail(String email);
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
